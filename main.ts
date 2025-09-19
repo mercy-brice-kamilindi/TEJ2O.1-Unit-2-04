a9ff2ae87cbb58f11445dd0bfb73fed114170df1
mercy-brice-kamilindi <mercy.brice.kamilindi@stu.ocsb.ca>
2025-09-19 13:30:57 -0400

    shows temperature

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mercy
+ * Created on: Sep 2025
+ * This program shows tempeture
 */
 
-basic.showString('Hello, World!')
+// variable tempeture
+let temperature : number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// comment
+input.onButtonPressed(Button.A, function() {
+    let temperature = input.temperature()
+    basic.showString('Temperature is')
+    basic.showNumber(temperature)
+    basic.showString('C')
+})
